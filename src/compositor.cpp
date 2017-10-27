1d26c02add280801a0ce9c6efd2f364800393cfd
Zachary Michaels <mikezackles@gmail.com>
2017-10-27 16:04:54 -0700

    Integrate EGL contexts with display management/buffer swapping code

--- src/compositor.cpp
+++ src/compositor.cpp
@@ -3,6 +3,7 @@
 #include <memory>
 #include <optional>
 #include <set>
+#include <thread>
 
 #include <cassert>
 #include <cstdlib>
@@ -47,6 +48,7 @@ namespace {
     }
     FileDescriptor &operator=(FileDescriptor const &) = delete;
     FileDescriptor &operator=(FileDescriptor &&other) noexcept {
+      // This class is final, and nothing here can throw exceptions.
       if (&other == this) return *this;
       this->~FileDescriptor();
       new (this) FileDescriptor{std::move(other)};
@@ -461,6 +463,7 @@ namespace {
       {
         if (!mHandle) std::cerr << "Failed to create GBM surface" << std::endl;
       }
+      Surface() : mHandle{nullptr, &safe_delete} {}
 
       explicit operator bool() const { return mHandle != nullptr; }
 
@@ -489,6 +492,7 @@ namespace {
         other.mDisplay = EGL_NO_DISPLAY;
       }
       Display &operator=(Display &&other) noexcept {
+        // This class is final, and nothing here can throw exceptions.
         if (this == &other) return *this;
         this->~Display();
         new (this) Display{std::move(other)};
@@ -587,6 +591,7 @@ namespace {
         other.mContext = EGL_NO_CONTEXT;
       }
       Context &operator=(Context &&other) noexcept {
+        // This class is final, and nothing here can throw exceptions.
         if (this == &other) return *this;
         this->~Context();
         new (this) Context{std::move(other)};
@@ -624,6 +629,7 @@ namespace {
         };
         EGLContext share = EGL_NO_CONTEXT;
         if (shared_context != nullptr) {
+          // This shouldn't be nullptr, but it won't break if it is
           share = shared_context->mContext;
         }
         EGLContext context = eglCreateContext(
@@ -653,6 +659,7 @@ namespace {
         : mDisplay{other.mDisplay}, mSurface{other.mSurface}
       { other.mDisplay = EGL_NO_DISPLAY; other.mSurface = EGL_NO_SURFACE; }
       Surface &operator=(Surface &&other) {
+        // This class is final, and nothing here can throw exceptions.
         if (this == &other) return *this;
         this->~Surface();
         new (this) Surface{std::move(other)};
@@ -683,24 +690,25 @@ namespace {
       }
     };
 
-    class CurrentContext final {
+    class BoundContext final {
     private:
       EGLDisplay mDisplay;
-      CurrentContext(EGLDisplay display) : mDisplay{display} {}
+      BoundContext(EGLDisplay display) : mDisplay{display} {}
     public:
-      CurrentContext() : mDisplay{EGL_NO_DISPLAY} {}
-      CurrentContext(CurrentContext const &) = delete;
-      CurrentContext &operator=(CurrentContext const &) = delete;
-      CurrentContext(CurrentContext &&other)
+      BoundContext() : mDisplay{EGL_NO_DISPLAY} {}
+      BoundContext(BoundContext const &) = delete;
+      BoundContext &operator=(BoundContext const &) = delete;
+      BoundContext(BoundContext &&other) noexcept
         : mDisplay{other.mDisplay}
       { other.mDisplay = EGL_NO_DISPLAY; }
-      CurrentContext &operator=(CurrentContext &&other) {
+      BoundContext &operator=(BoundContext &&other) noexcept {
+        // This class is final, and nothing here can throw exceptions.
         if (this == &other) return *this;
-        this->~CurrentContext();
-        new (this) CurrentContext{std::move(other)};
+        this->~BoundContext();
+        new (this) BoundContext{std::move(other)};
         return *this;
       }
-      ~CurrentContext() {
+      ~BoundContext() {
         if (*this) eglMakeCurrent(
           mDisplay, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT
         );
@@ -709,7 +717,7 @@ namespace {
       explicit operator bool() const { return mDisplay != nullptr; }
 
       // This function creates global, thread-local state! See ThreadContext.
-      static CurrentContext create(
+      static BoundContext create(
         Display const &display, Surface const &surface, Context const &context
       ) {
         EGLBoolean success = eglMakeCurrent(
@@ -721,79 +729,201 @@ namespace {
         }
         return {display.get()};
       }
+
+      static BoundContext create(
+        Display const &display, Context const &context
+      ) {
+        // Something is wrong if this thread already has a context bound
+        assert(eglGetCurrentContext() == EGL_NO_CONTEXT);
+
+        EGLBoolean success = eglMakeCurrent(
+          display.get(), EGL_NO_SURFACE, EGL_NO_SURFACE, context.get()
+        );
+        if (!success) {
+          std::cerr << "Failed to make context current" << std::endl;
+          return {};
+        }
+        return {display.get()};
+      }
     };
 
-    class ThreadContext {
+    class DrawableContext {
     private:
-      // Mimic what OpenGL does
-      static thread_local CurrentContext sCurrentContext;
+      Context mContext;
+      Surface mSurface;
+      BoundContext mBoundContext;
+
+      DrawableContext(
+        Context context
+      , Surface surface
+      , BoundContext bound
+      ) : mContext{std::move(context)}
+        , mSurface{std::move(surface)}
+        , mBoundContext{std::move(bound)}
+      {}
 
     public:
-      // This function creates global, thread-local state!
-      static Context make_current(
-        Display const &display, gbm::Surface const &gbm_surface
-      , Context const *share_context = nullptr
+      DrawableContext() = default;
+
+      static DrawableContext create(
+        Display const &display
+      , gbm::Surface const &gbm_surface
+      , Context const *shared = nullptr
       ) {
         EGLConfig config = find_config(display);
-        if (config == nullptr) return {};
+        if (!config) return {};
 
-        auto context = Context::create(display, config, share_context);
+        auto context = Context::create(display, config, shared);
         if (!context) return {};
 
         auto surface = Surface::create(display, config, gbm_surface);
         if (!surface) return {};
 
-        sCurrentContext = CurrentContext::create(display, surface, context);
-        if (!sCurrentContext) return {};
+        auto bound = BoundContext::create(display, surface, context);
+        if (!bound) return {};
 
-        return context;
+        return {std::move(context), std::move(surface), std::move(bound)};
       }
 
-      static bool is_set() {
-        return static_cast<bool>(sCurrentContext);
+      explicit operator bool() const { return static_cast<bool>(mContext); }
+
+      void swap_buffers(Display const &display) {
+        assert(*this);
+        eglSwapBuffers(display.get(), mSurface.get());
       }
     };
-    thread_local CurrentContext ThreadContext::sCurrentContext{};
+
+    class SurfacelessContext {
+    private:
+      Context mContext;
+      BoundContext mBoundContext;
+
+      SurfacelessContext(
+        Context context
+      , BoundContext bound
+      ) : mContext{std::move(context)}
+        , mBoundContext{std::move(bound)}
+      {}
+
+    public:
+      SurfacelessContext() = default;
+
+      static SurfacelessContext create(Display const &display) {
+        EGLConfig config = find_config(display);
+        if (!config) return {};
+
+        auto context = Context::create(display, config);
+        if (!context) return {};
+
+        auto bound = BoundContext::create(display, context);
+        if (!bound) return {};
+
+        return {std::move(context), std::move(bound)};
+      }
+
+      explicit operator bool() const { return static_cast<bool>(mContext); }
+
+      // Call this on another thread!
+      DrawableContext create_child_context(
+        Display const &display, gbm::Surface const &gbm_surface
+      ) const {
+        assert(*this);
+        return DrawableContext::create(display, gbm_surface, &mContext);
+      }
+
+      //class RAIIThread {
+      //private:
+      //  std::thread mThread;
+      //public:
+      //  RAIIThread() = default;
+      //  template <typename ...Args>
+      //  RAIIThread(Args&&... args) : mThread{std::forward<Args>(args)...} {}
+      //  RAIIThread(RAIIThread const &) = delete;
+      //  RAIIThread &operator=(RAIIThread const &) = delete;
+      //  RAIIThread(RAIIThread &&) = default;
+      //  RAIIThread &operator=(RAIIThread &&) = default;
+      //  ~RAIIThread() { if (mThread.joinable()) mThread.join(); }
+
+      //  explicit operator bool() const { return mThread.joinable(); }
+      //};
+
+      //template <typename Callback>
+      //RAIIThread spawn_child_thread(Callback callback) {
+      //  return [callback = std::move(callback)]() {
+      //    callback();
+      //  };
+      //}
+    };
   }
 
+  // Instances of this class contain implicit global, thread-local state due
+  // to the nature of the EGL/OpenGL APIs. It should not be moved across
+  // thread boundaries.
   class Display {
   private:
+    std::thread::id mThreadID;
     gbm::Surface mSurface;
+    egl::DrawableContext mEGL;
     uint32_t mCrtcID;
-    uint32_t mConnectorID;
-    drmModeModeInfo *mMode;
     gbm::FrontBuffer mCurrentFrontBuffer;
     gbm::FrontBuffer mNextFrontBuffer;
     bool mWaitingForPageFlip;
-  public:
+
     Display(
-      gbm::Surface surface
-    , uint32_t crtc_id, uint32_t connector_id
-    , drmModeModeInfo &mode
-    ) : mSurface{std::move(surface)}, mCrtcID{crtc_id}
-      , mConnectorID{connector_id}, mMode{&mode}
+      gbm::Surface gbm_surface
+    , egl::DrawableContext context
+    , uint32_t crtc_id
+    ) : mThreadID{std::this_thread::get_id()}
+      , mSurface{std::move(gbm_surface)}
+      , mEGL{std::move(context)}
+      , mCrtcID{crtc_id}
       , mCurrentFrontBuffer{}, mNextFrontBuffer{}
       , mWaitingForPageFlip{false}
     {}
 
+    Display() : Display{{}, {}, 0} {}
+  public:
+    static Display create(
+      gbm::Device const &gbm, egl::Display const &egl
+    , egl::SurfacelessContext const &master_context
+    , uint32_t width, uint32_t height
+    , uint32_t crtc_id // TODO - needed?
+    ) {
+      gbm::Surface gbm_surface{gbm, width, height};
+      if (!gbm_surface) return {};
+
+      egl::DrawableContext context = master_context.create_child_context(
+        egl, gbm_surface
+      );
+      if (!context) return {};
+
+      return {std::move(gbm_surface), std::move(context), crtc_id};
+    }
+
     explicit operator bool() const {
-      return
-          static_cast<bool>(mSurface)
-       && mMode != nullptr
-       && egl::ThreadContext::is_set()
+      return static_cast<bool>(mSurface)
+          // Prevent using this on a thread other than the one it was created on
+          && std::this_thread::get_id() == mThreadID;
       ;
     }
 
     uint32_t crtc_id() const { assert(*this); return mCrtcID; }
 
-    bool set_mode(drm::Descriptor const &gpu) {
+    bool set_mode(
+      drm::Descriptor const &gpu
+    , egl::Display const &egl_display
+    , uint32_t connector_id
+    , drmModeModeInfo &mode
+    ) {
       assert(*this);
-      // TODO - eglSwapBuffers!
+      glClearColor(0.5, 0.5, 0.5, 1.0);
+      glClear(GL_COLOR_BUFFER_BIT);
+      mEGL.swap_buffers(egl_display);
       auto front = mSurface.lock_front_buffer();
       if (!front) return false;
       auto framebuffer = front.ensure_framebuffer(gpu);
       if (!framebuffer) return false;
-      if (drm::set_mode(gpu, *framebuffer, mConnectorID, mCrtcID, *mMode)) {
+      if (drm::set_mode(gpu, *framebuffer, connector_id, mCrtcID, mode)) {
         mCurrentFrontBuffer = std::move(front);
         return true;
       } else {
@@ -801,9 +931,12 @@ namespace {
       }
     }
 
-    bool begin_swap_buffers(drm::Descriptor const &gpu) {
-      assert(*this);
-      assert(mCurrentFrontBuffer);
+    bool begin_swap_buffers(
+      drm::Descriptor const &gpu
+    , egl::Display const &egl_display
+    ) {
+      assert(*this && mCurrentFrontBuffer);
+      mEGL.swap_buffers(egl_display);
       auto front = mSurface.lock_front_buffer();
       if (!front) return false;
       auto framebuffer = front.ensure_framebuffer(gpu);
@@ -837,6 +970,8 @@ namespace {
   private:
     drm::Descriptor mGPUDescriptor;
     gbm::Device mGBM;
+    egl::Display mEGL;
+    egl::SurfacelessContext mMasterContext;
     // The keys here are connector ids returned from libdrm. The hope is that
     // they are consistent across reboots etc.
     std::map<uint32_t, Display> mDisplayLookup;
@@ -858,9 +993,14 @@ namespace {
       return std::nullopt;
     }
 
-    DeviceManager(drm::Descriptor gpu, std::set<uint32_t> unused_crtcs)
-      : mGPUDescriptor{std::move(gpu)}
-      , mGBM{mGPUDescriptor}
+    DeviceManager(
+      drm::Descriptor gpu, gbm::Device gbm, egl::Display egl
+    , egl::SurfacelessContext master
+    , std::set<uint32_t> unused_crtcs
+    ) : mGPUDescriptor{std::move(gpu)}
+      , mGBM{std::move(gbm)}
+      , mEGL{std::move(egl)}
+      , mMasterContext{std::move(master)}
       , mDisplayLookup{}
       , mUnusedCrtcs{std::move(unused_crtcs)}
     {}
@@ -873,10 +1013,25 @@ namespace {
       drm::Resources resources{*gpu};
       if (!resources) return std::nullopt;
 
+      gbm::Device gbm{*gpu};
+      if (!gbm) return std::nullopt;
+
+      auto egl = egl::Display::create(gbm);
+      if (!egl) return std::nullopt;
+
+      auto master = egl::SurfacelessContext::create(egl);
+      if (!master) return std::nullopt;
+
       std::set<uint32_t> unused_crtcs{};
       for (uint32_t crtc_id : resources.crtcs()) unused_crtcs.insert(crtc_id);
 
-      return DeviceManager{std::move(*gpu), std::move(unused_crtcs)};
+      std::optional<DeviceManager> result{{
+        std::move(*gpu), std::move(gbm), std::move(egl)
+      , std::move(master), std::move(unused_crtcs)
+      }};
+      if (!*result) return std::nullopt;
+
+      return result;
     }
 
     explicit operator bool() const { return static_cast<bool>(mGPUDescriptor); }
@@ -908,16 +1063,14 @@ namespace {
           auto crtc_id = find_crtc_for_connector(resources, connector);
           if (!crtc_id) continue;
 
-          gbm::Surface surface{mGBM, mode->hdisplay, mode->vdisplay};
-          if (!surface) continue;
-
-          mDisplayLookup.emplace(
-            std::piecewise_construct
-          , std::forward_as_tuple(connector.id())
-          , std::forward_as_tuple(
-              std::move(surface), *crtc_id, connector.id(), *mode
-            )
+          auto display = Display::create(
+            mGBM, mEGL, mMasterContext, mode->hdisplay, mode->vdisplay
+          , *crtc_id
           );
+          if (!display) continue;
+
+          // This isn't working code yet. We need a thread per display.
+          mDisplayLookup.emplace(connector.id(), std::move(display));
           mUnusedCrtcs.erase(*crtc_id);
         }
       }
